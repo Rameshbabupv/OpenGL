f8f3e5e6cc515cb2019096d5b105c296d6bef892
sciencelord01 <adharsh.babu@gmail.com>
2015-05-23 17:46:28 -0400

    Added simplified_basic_shapes.cpp

---
+++ simplified_basic_shapes.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <stdlib.h>
+
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+
+
+void draw(){
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+	glLoadIdentity(); //Reset the drawing perspective
+	
+	glBegin(GL_QUADS); //Begin quadrilateral coordinates
+	
+	//Trapezoid
+	glVertex3f(-0.7f, -1.5f, -5.0f);
+	glVertex3f(0.7f, -1.5f, -5.0f);
+	glVertex3f(0.4f, -0.5f, -5.0f);
+	glVertex3f(-0.4f, -0.5f, -5.0f);
+	
+	glEnd(); //End quadrilateral coordinates
+	
+	glBegin(GL_TRIANGLES); //Begin triangle coordinates
+	
+	//Pentagon
+	glVertex3f(0.5f, 0.5f, -5.0f);
+	glVertex3f(1.5f, 0.5f, -5.0f);
+	glVertex3f(0.5f, 1.0f, -5.0f);
+	
+	glVertex3f(0.5f, 1.0f, -5.0f);
+	glVertex3f(1.5f, 0.5f, -5.0f);
+	glVertex3f(1.5f, 1.0f, -5.0f);
+	
+	glVertex3f(0.5f, 1.0f, -5.0f);
+	glVertex3f(1.5f, 1.0f, -5.0f);
+	glVertex3f(1.0f, 1.5f, -5.0f);
+	
+	//Triangle
+	glVertex3f(-0.5f, 0.5f, -5.0f);
+	glVertex3f(-1.0f, 1.5f, -5.0f);
+	glVertex3f(-1.5f, 0.5f, -5.0f);
+	
+	glEnd(); //End triangle coordinates
+	
+	glutSwapBuffers(); //Send the 3D scene to the screen
+
+}
+
+
+void handleKeypress(unsigned char key, //The key that was pressed
+					int x, int y) {    //The current mouse coordinates
+	switch (key) {
+		case 27: //Escape key
+			exit(0); //Exit the program
+	}
+}
+
+
+void handleResize(int w, int h) {
+	//Tell OpenGL how to convert from coordinates to pixel values
+	glViewport(0, 0, w, h);
+	
+	glMatrixMode(GL_PROJECTION); //Switch to setting the camera perspective
+	
+	//Set the camera perspective
+	glLoadIdentity(); //Reset the camera
+	gluPerspective(45.0,                  //The camera angle
+	(double)w / (double)h, //The width-to-height ratio
+				   1.0,                   //The near z clipping coordinate
+				   200.0);                //The far z clipping coordinate
+}
+
+
+int main( int argc, char** argv ){
+
+	// Initialize	
+	glutIntialize( &argc, argv );
+	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+	glutInitWindowSize( 400, 400 );
+	glutInitWindowPosition(200, 200);
+
+	glutCreateWindow("simplified_basic_shapes.cpp");
+	glEnable(GL_DEPTH_TEST);
+
+	glutDisplayFunc(draw);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	
+	glutMainLoop(); 
+	return 0; 
+
+
+
+}
